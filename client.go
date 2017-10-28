00e4a9098c2dcfb9fc64e397427e7617bc63690c
Juha Harviainen <juha.harviainen@gmail.com>
2017-10-28 13:50:51 +0300

    Move most of main.go's contents to client.go

--- client.go
+++ client.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 	"net"
+	"strings"
+	"bufio"
 )
 
 type Client struct {
 	nick string
 	channels []string
 	conn net.Conn
+	logger *Logger
 }
 
 func send(conn net.Conn, msg string) {
@@ -21,6 +24,41 @@ func (cli *Client) ChangeNick(nick string) { // TODO: add error handling for una
 	send(cli.conn, "NICK " + nick)
 }
 
+func (cli *Client) Auth(text string) {
+	send(cli.conn, "USER " + text)
+}
+
 func (cli *Client) Pong(target string) {
 	send(cli.conn, "PONG " + target)
+}
+
+func (cli *Client) Run() {
+	reader := bufio.NewReader(cli.conn)
+	for {
+		result, err := reader.ReadString('\n')
+		for result[len(result) - 1] == '\r' || result[len(result) - 1] == '\n' {
+			result = result[:len(result) - 1]
+		}
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("<- %v\n", result)
+		if result[:4] == "PING" {
+			cli.Pong(result[5:])
+			//send(conn, "PONG " + result[5:len(result) - 1])
+		} else {
+			spl := strings.Split(result, " ")
+			command := spl[1]
+			if command == "PRIVMSG" { // We're not currently interested in commands other than PRIVMSGs
+				sender := strings.Split(spl[0], "!")
+				nick := sender[0][1:]
+				receiver := spl[2]
+				indexOfColon := strings.Index(result[1:], ":")
+				if indexOfColon != -1 {
+					msg := result[indexOfColon + 2:]
+					cli.logger.Log(receiver + " " + nick + " " + msg)
+				}
+			}
+		}
+	}	
 }
--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"net"
-	"bufio"
-	"strings"
 )
 
 const NICK string = "quha"
 const SERVER string = "chat.freenode.net:8000"
-const CHANNEL string = "#datatahti"
 
 const LOG_PATH string = "girkki.log" 
 
 func main() {
+	var logger Logger
+	logger.Open(LOG_PATH)
+	defer logger.Close()
+
 	conn, dialErr := net.Dial("tcp", SERVER)
 	if dialErr != nil {
 		panic(dialErr)
@@ -21,44 +21,12 @@ func main() {
 
 	client := Client{
 		NICK,
-		[]string{CHANNEL},
+		[]string{},
 		conn,
+		&logger,
 	}
 
-	var logger Logger
-	logger.Open(LOG_PATH)
-	defer logger.Close()
-
-	
-	//send(conn, "NICK " + NICK)
 	client.ChangeNick(NICK)
-	send(conn, "USER " + NICK + " 8 * :Kuha")
-	reader := bufio.NewReader(conn)
-	for {
-		result, err := reader.ReadString('\n')
-		for result[len(result) - 1] == '\r' || result[len(result) - 1] == '\n' {
-			result = result[:len(result) - 1]
-		}
-		if err != nil {
-			panic(err)
-		}
-		fmt.Printf("<- %v\n", result)
-		if result[:4] == "PING" {
-			client.Pong(result[5:])
-			//send(conn, "PONG " + result[5:len(result) - 1])
-		} else {
-			spl := strings.Split(result, " ")
-			command := spl[1]
-			if command == "PRIVMSG" { // We're not currently interested in commands other than PRIVMSGs
-				sender := strings.Split(spl[0], "!")
-				nick := sender[0][1:]
-				receiver := spl[2]
-				indexOfColon := strings.Index(result[1:], ":")
-				if indexOfColon != -1 {
-					msg := result[indexOfColon + 2:]
-					logger.Log(SERVER + " " + receiver + " " + nick + " " + msg)
-				}
-			}
-		}
-	}
+	client.Auth(NICK + " 8 * :Kuha")
+	client.Run()
 }
